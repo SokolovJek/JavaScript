9aa92bf5596a190e23de0000b09b6109b81036ed
SokolovJek <sokolovgek@mail.ru>
2021-11-21 13:56:42 +0300

    Урок 7. Урок-практикум

---
+++ lesson7/2 выполните практические задания/3 уровень/1-tabs/app.js
@@ -0,0 +1,58 @@
+'use strict';
+const texts = {
+    text1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    text2: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.',
+    text3: 'Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил.'
+};
+
+/* 
+1. Получите ссылку на .text, например с помощью querySelector
+2. Получите коллекцию, в которой хранятся все .nav-link, например с помощью querySelectorAll
+    2.1 Переберите полученную коллекцию, например с помощью forEach, и каждой ссылке назначьте
+    обработчик клика функцию clickHandler.
+*/
+
+
+/**
+ * Обработчик клика по .nav-link
+ * @param {MouseEvent} event 
+ */
+function clickHandler(event) {
+    changeActiveClass(event)
+    changeText(event)
+}
+
+/**
+ * Эта функция должна убирать .active у предыдущего .nav-link и ставить его
+ * на тот, по которому кликнули.
+ * @param {MouseEvent} event 
+ */
+function changeActiveClass(event) {
+    let oldEl = document.querySelector('.active');
+    oldEl.classList.remove('active');
+    let newEl = event.target;
+    newEl.classList.add('active');
+}
+
+/**
+ * Эта фукнция должна читать текст (например через textContent) из 
+ * .nav-link по которому кликнули и в зависимости от этого в .text
+ * ставить соответствующий текст из texts.
+ * @param {MouseEvent} event 
+ */
+function changeText(event) {
+    let newEl = event.target;
+    let nameEl = newEl.innerHTML;
+    switch (nameEl) {
+        case 'Link 1':
+            divEl.innerHTML = texts.text1;
+            break;
+        case 'Link 2':
+            divEl.innerHTML = texts.text2;
+            break;
+        case 'Link 3':
+            divEl.innerHTML = texts.text3;
+            break;
+    }
+
+}
